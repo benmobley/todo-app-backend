25b5ae28d9c4abdf563032dc17d7771ed2f993a4
benmobley <benjaminmobley4@gmail.com>
2024-12-04 13:56:47 -0600

    added user functionality for categories/todos

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,13 +2,17 @@ class CategoriesController < ApplicationController
   before_action :authenticate_user
 
   def index
-    @categories = Category.all
+    @categories = current_user.categories
     render :index
   end
 
   def show
     @category = Category.find_by(id: params[:id])
-    render :show
+    if current_user.id == @category.user_id
+      render :show
+    else
+      render json: { message: "Category does not exist for current user" }
+    end
   end
 
   def create
@@ -21,15 +25,23 @@ class CategoriesController < ApplicationController
 
   def update
     @category = Category.find_by(id: params[:id])
-    @category.update(
-      name: params[:name] || @category.name,
-    )
-    render :show
+    if current_user.id == @category.user_id
+      @category.update(
+        name: params[:name] || @category.name,
+      )
+      render :show
+    else
+      render json: { message: "Category does not exist for current user" }
+    end
   end
 
   def destroy
     @category.find_by(id: params[:id])
-    @category.destroy
-    render json: { message: "sucessfully destroyed" }
+    if current_user.id == @category.user_id
+      @category.destroy
+      render json: { message: "sucessfully destroyed" }
+    else
+      render json: { message: "Category does not exist for current user" }
+    end
   end
 end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,7 @@ class TodosController < ApplicationController
   before_action :authenticate_user
 
   def index
-    @todos = Todo.all
+    @todos = current_user.todos
     render :index
   end
 
@@ -20,24 +20,36 @@ class TodosController < ApplicationController
 
   def show
     @todo = Todo.find_by(id: params[:id])
-    render :show
+    if current_user.id == @todo.user_id
+      render :show
+    else
+      render json: { message: "Todo doesn't exist for current user" }
+    end
   end
 
   def update
     @todo = Todo.find_by(id: params[:id])
-    @todo.update(
-      category_id: params[:category_id] || @todo.category_id,
-      title: params[:title] || @todo.title,
-      description: params[:description] || @todo.description,
-      deadline: params[:deadline] || @todo.deadline,
-      completed: params[:completed] || @todo.completed,
-    )
-    render :show
+    if current_user.id == @todo.user_id
+      @todo.update(
+        category_id: params[:category_id] || @todo.category_id,
+        title: params[:title] || @todo.title,
+        description: params[:description] || @todo.description,
+        deadline: params[:deadline] || @todo.deadline,
+        completed: params[:completed] || @todo.completed,
+      )
+      render :show
+    else
+      render json: { message: "Todo doesn't exist for current user" }
+    end
   end
 
   def destroy
     @todo = Todo.find_by(id: params[:id])
-    @todo.destroy
-    render json: { message: "sucessfully destroyed" }
+    if current_user.id == @todo.user_id
+      @todo.destroy
+      render json: { message: "sucessfully destroyed" }
+    else
+      render json: { message: "Todo doesn't exist for current user" }
+    end
   end
 end
